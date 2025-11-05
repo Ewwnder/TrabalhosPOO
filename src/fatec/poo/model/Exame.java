7b4231266fb563b995ebf20d486004c99319bd27
LucasMorais42 <lucasmorais2214@gmail.com>
2025-11-05 14:49:02 -0300

    :sparkles: feat(exame): envio model exame

---
+++ src/fatec/poo/model/Exame.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+/**
+ *
+ * @author LucasMorais
+ */
+public class Exame {
+    private int codigo;
+    private String descricao;
+    private String data;
+    private String horario;
+    private double valor;
+    private Consulta consulta;
+
+    public Exame(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+    
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    
+}
